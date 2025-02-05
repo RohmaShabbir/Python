2313e24d4d21b2309fac0847d03643410e75e446
Rohma-Shabbir <168419231+RohmaShabbir@users.noreply.github.com>
2025-02-05 20:00:41 +0500

    Add files via upload

---
+++ Variables & Data Types/Practice.py
@@ -0,0 +1,30 @@
+#  Practice Question:
+
+# 1: Write a program to input 2 numbers & print their sum.
+
+first = int(input('enter first:'))
+second = int(input('enter second:'))
+
+print('sum =', first + second)
+
+# 2: WAP to input side of a square & print it's area.
+
+side = float(input('enter square side:'))
+
+print('area =', side * side)
+
+
+# 3: WAP to input 2 floating points number & print their average.
+
+a = float(input('enter first:'))
+b = float(input('enter second:'))
+
+print('avg = ', ( a + b )/ 2)
+
+# 4: WAP to input 2 int numbers , a and b.
+#    Print true if ais greater than or equal to b. if not print 2.
+
+a = int(input('enter first:'))
+b = int(input('enter second:'))
+
+print(a >= b)
