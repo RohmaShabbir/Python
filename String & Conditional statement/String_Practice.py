aa089754c1d40b976a522744be5e2b957e39acd5
Rohma-Shabbir <168419231+RohmaShabbir@users.noreply.github.com>
2025-02-12 19:37:24 +0500

    Add files via upload

---
+++ String & Conditional statement/String_Practice.py
@@ -0,0 +1,16 @@
+# WAP the input user's first name & print it's length.
+
+name = input ("Enter your name:")
+print("Length of your name is:" , len(name))
+# Output: Enter your name:python
+#         Length of your name is: 6
+
+# <----------------->
+
+#  WAP to find the occurrance of '$' in a string
+
+str = "Hello, $I'm the $ symbol"
+print(str.count("$"))
+# Output: 2
+
+
