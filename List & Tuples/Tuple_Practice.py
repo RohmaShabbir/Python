ee871ac07d33fd7d6545f152504083e87162ed3e
Rohma-Shabbir <168419231+RohmaShabbir@users.noreply.github.com>
2025-02-19 17:07:18 +0500

    Add files via upload

---
+++ List & Tuples/Tuple_Practice.py
@@ -0,0 +1,10 @@
+# WAP to count the number of student with the "A" grade in the following tuple.
+
+grade = ("A", "B", "C", "D", "B", "A", "A")
+print(grade.count("A"))
+
+# Store the above value in a list & sort them from "A" to "D".
+
+list = ["A", "B", "C", "D", "B", "A", "A"]
+list.sort()
+print(list)
