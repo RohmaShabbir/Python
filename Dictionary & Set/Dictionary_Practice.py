71662f8909f0b28efbd0e5b7ce3ca02c5662fd78
Rohma-Shabbir <168419231+RohmaShabbir@users.noreply.github.com>
2025-02-24 13:59:25 +0500

    Add files via upload

---
+++ Dictionary & Set/Dictionary_Practice.py
@@ -0,0 +1,15 @@
+# Practice Questions
+
+# 1: Store following words in a python dictionary:
+dictionary = {
+    "cat" : "a small animal",
+    "table": ["a piece of furniture", "list of shair & table"]
+}
+print(dictionary)
+
+# 2: You are given a list of subject for student. Assume one class is required for 1 subject. how many classes are needed by all students.
+
+Subject = {
+    "Python", "C++", "Java", "C++", "TypeScript", "Python", "Java", "C"
+}
+print(len(Subject))
